df1d6b933a195c7784673a256999d3c04098ffa9
SumDim <saikam@hotmail.de>
2014-05-20 19:46:09 +0200

    Create datesForContributors.js

---
+++ datesForContributors.js
@@ -0,0 +1,75 @@
+{
+
+    var ceBitTeilnehmer= { "CB":[
+    {
+        name:"Ent",
+        vorname:"Thilo",
+        studiengang:"IB",
+        Email:"t.ent@gmail.de",
+        Messe:"CeBit"
+    },
+    {
+        name:"Kraus",
+        vorname:"Martina",
+        studiengang:"IB",
+        Email:"m.kraushs-mannheim.de",
+        Messe:"CeBit"
+    },
+    {
+        name:"Smits",
+        vorname:"Thomas",
+        studiengang:"UIB",
+        Email:"t.smits@hs-mannheim.de",
+        Messe:"CeBit"
+    },
+    {
+        name:"Groeschel",
+        vorname:"Michael",
+        studiengang:"UIB",
+        Email:"m.groeschel@hs-mannheim.de",
+        Messe:"CeBit"
+    }]}
+    var conHitTeilnehmer= { "CH":[
+        {
+        name:"Schmuecker",
+        vorname:"Paul",
+        studiengang:"IMB",
+        Email:"p.schmuecker@hs-mannheim.de",
+        Messe:"ConHit"
+    },
+    
+    {
+
+        name:"Felsenheimer",
+        vorname:"Jan",
+        studiengang:"IB",
+        Email:"j.felsenheimer@diebestebaendderwelt.de",
+        Messe:"ConHit"
+    }]}
+
+    var webTechConTeilnehmer= { "WTC":[
+        {
+        name:"Spies",
+        vorname:"Marcell",
+        studiengang:"IB",
+        Email:"m.Spies@gmail.de",
+        Messe:"WebTechCon"
+    },
+    
+    {
+
+        name:"Hoppe",
+        vorname:"Johannes",
+        studiengang:"UIB",
+        Email:"j.hoppe@gmx.de",
+        Messe:"WebTechCon"
+    },
+    {
+
+        name:"Hoffmeister",
+        vorname:"Johannes",
+        studiengang:"UIB",
+        Email:"cessor@gmail.de",
+        Messe:"WebTechCon"
+    }]}
+}
